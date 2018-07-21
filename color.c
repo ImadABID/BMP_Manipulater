2d50ba623f89139d496daa019b481b9e7bfe6632
imadabid <imad.abied@gmail.com>
2018-07-21 20:17:32 +0100

    adding 2 function: set_header_image and set_footer_image

--- color.c
+++ color.c
@@ -2,3 +2,26 @@
 
 
 
+void set_header_image( int l, int h){
+    {char data[18]={66,77,58,0,0,0,0,0,0,0,54,0,0,0,40,0,0,0};
+    fwrite(data,sizeof(char),18,fic);}
+
+    fwrite(&l,2,1,fic);
+    {int x=0; fwrite(&x,1,2,fic);}
+    fwrite(&h,2,1,fic);
+
+    {
+        char data[30]={0,0,1,0,24,0,0,0,0,0,4,
+        0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,0};
+        fwrite(data,sizeof(char),30,fic);
+    }
+}
+
+void set_footer_image(int l, int h){
+    int i;
+    int x=0;
+    for(i=0;i<l*h;i++){
+        fwrite(&x,sizeof(char),1,fic);
+    }
+}
--- color.h
+++ color.h
@@ -1,6 +1,14 @@
 #ifndef __COLOR_H__
 #define __COLOR_H__
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+FILE *fic;
+
+void set_header_image( int l, int h);
+void set_footer_image(int l, int h);
 
 
 #endif
