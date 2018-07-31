71eada45f3569e7f8e49631a88262c8f62e2eecd
imadabid <imad.abied@gmail.com>
2018-07-31 02:05:15 +0100

    New function: DtoH

--- binary.c
+++ binary.c
@@ -8,4 +8,14 @@ unsigned int HtoD(unsigned char *p){
 unsigned char *DtoH(unsigned int *e){
     unsigned char *p=malloc(4);
     p[3]=(int)(e/_256P3);
+    int e-=p[3]*_256P3;
+
+    p[2]=(int)(e/_256P2);
+    int e-=p[2]*_256P2;
+
+    p[1]=(int)(e/256);
+    int e-=p[1]*256;
+
+    p[0]=e;
+    return p;
 }
