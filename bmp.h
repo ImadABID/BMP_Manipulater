8252d0e192f87cae72bb4bb8db2711380133d1d0
imadabid <imad.abied@gmail.com>
2018-08-01 01:41:13 +0100

    transfer functions from bmp.h to paint.h

--- bmp.h
+++ bmp.h
@@ -9,12 +9,6 @@ typedef enum {
     false,true
 }bool;
 
-typedef struct structure{
-    unsigned char o1;
-    unsigned char o2;
-    unsigned char o3;
-}color;
-
 typedef struct structure2{
     char *path;
     int size;
@@ -22,19 +16,12 @@ typedef struct structure2{
     int h;
 }image;
 
-typedef struct structure3{
-    image img;
-    int x;
-    int y;
-    color couleur;
-}pixel;
+image set_image(char *img_path, int img_size, int lenght, int hight);
 
 image open(char *path);
 
-image set_image(char *img_path, int img_size, int lenght, int hight);
 void set_header_image(image img);
 void set_footer_image(image img);
 
 unsigned char *adapt(unsigned char *p);
 
-color rgb(unsigned char o1, unsigned char o2, unsigned char o3);
