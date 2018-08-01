ed3a5cd911513b617ef0ae1c88d446dde5437ff4
imadabid <imad.abied@gmail.com>
2018-08-01 01:18:36 +0100

    adding comment in binary library

--- binary.h
+++ binary.h
@@ -8,6 +8,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ * We mean by hexadicimal in this library an array
+ * of caracter every carcter represent an oct if
+ * we stock it using fwrite() we are going
+ * to have the hexadecimal. 
+ */
+
 unsigned int HtoD(unsigned char *p);
 unsigned char *DtoH(unsigned int e);
 
