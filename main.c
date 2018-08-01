eea362f935d77f10d8f9c3dd3924e27b5e143e37
imadabid <imad.abied@gmail.com>
2018-08-01 01:14:33 +0100

    testing & bug fix of DtoH

--- main.c
+++ main.c
@@ -2,25 +2,8 @@
 #include "binary.h"
 
 int main(){
-    
-    unsigned char *p=malloc(4); p=DtoH(129);
-    
-    printf("%d\t",p[0]);
-    printf("%d\t",p[1]);
-    printf("%d\t",p[2]);
-    printf("%d\t",p[3]);
 
-    p[0]=129;
-    p[1]=0;
-    p[2]=0;
-    p[3]=0;
 
-    
-    printf("\n%d",HtoD(p));
-    unsigned int x= 126*16777216;
-   // printf("\n%d",x);
-
-    printf("\n%d",sizeof(unsigned int));
     return 0;
 }
 
