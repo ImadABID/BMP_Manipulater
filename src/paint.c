2bd5d9e83a7eb36527f5dd23db889868c670845a
imadabid <imad.abied@gmail.com>
2018-08-02 01:05:11 +0100

    New function: from2Dto1D

--- src/paint.c
+++ src/paint.c
@@ -5,3 +5,12 @@ color rgb(unsigned char o1, unsigned char o2, unsigned char o3){
     c.o1=o3; c.o2=o2; c.o3=o1;
     return c;
 }
+
+void save_pixel(pixel px){
+
+}
+
+unsigned int from2Dto1D(pexel px){
+    px.y=image.h-px.y;
+    return (54+(px.y-1)*px.image.l+px.x);
+} 
