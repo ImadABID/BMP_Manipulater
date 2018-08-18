af25ca4c7fb3dd5930458f8a6537d57bdcb110f0
imadabid <imad.abied@gmail.com>
2018-08-18 17:18:33 +0100

    using the new functions in add_rect

--- exemples/add_rect.c
+++ exemples/add_rect.c
@@ -1,18 +1,24 @@
 #include "D:\Imad_Donnes\Etude\Programmation\Projets\BMP_Manipulater\src\paint.h"
+#include <stdio.h>
 
 int main(){
-    image img=open("french_flag.bmp");
+    printf("Opening image ...\n");
+    image img=open("image.bmp");
 
-    pixel px; px.img= img; px.couleur=rgb(128,255,128);
+    printf("treating image ...\n");
+    pixel px; px.img= img; px.couleur=rgb(200,255,200);
 
     int x,y;
     for(x=(int)(img.l/3);x<(int)(img.l*2/3);x++){
         px.x=x;
         for(y=(int)(img.h/3);y<(int)(img.h*2/3);y++){
             px.y=y;
-            save_pixel(px);
+            put_pixel(px);
         }
     }
+
+    printf("saving image ...");
+    save_image(img);
 
     return 0;
 }
