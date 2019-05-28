5e48f49ef0edcbe1931a5226aff23158d36c77d7
ImadABID <imad.abied@gmail.com>
2019-05-28 01:21:48 +0100

    adding the class: Binnary

--- BMP_Manipulater++/binnary.hpp
+++ BMP_Manipulater++/binnary.hpp
@@ -1,4 +1,15 @@
 #if ! defined(__BINNARY__)
 #define(__BINNARY__)
-	
+
+#define _256P3 16777216
+#define _256P2 65536
+
+#include <stdlib.h>
+
+class Binnary{
+public:
+	unsigned int HtoD(unsigned char *p);
+	unsigned char *DtoH(unsigned int e);
+}
+
 #endif
