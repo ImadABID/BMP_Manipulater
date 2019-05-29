82c6b5e1208b3ea770bcb2082d1b064c74df5e0e
ImadABID <imad.abied@gmail.com>
2019-05-30 00:20:57 +0100

    deleting this key word

--- BMP_Manipulater++/Bmp.cpp
+++ BMP_Manipulater++/Bmp.cpp
@@ -1,10 +1,10 @@
 #include "Bmp.hpp"
 Bmp::Bmp(char *img_path, int lenght, int hight){
-	this::path=malloc(255); this::path=img_path;
+	path=malloc(255); path=img_path;
 
 
     if(lenght==-1 && hight==-1){ //image existe
-        FILE *f=fopen(this::path,"rb");
+        FILE *f=fopen(path,"rb");
         //size
         unsigned char *ps=malloc(4);
         fseek(f,2,SEEK_SET);
@@ -23,33 +23,33 @@ Bmp::Bmp(char *img_path, int lenght, int hight){
         fread(ph,1,4,f);
         ph=adapt(ph);
 
-        this::l=pl;
-        this::h=ph;
+        l=pl;
+        h=ph;
         this::size=ps;
-        fread(this::hex,this::size,1,f);
+        fread(hex,size,1,f);
         fclose(f);
     }else{ //create a new image
-        this::l= lenght;
-        this::h= hight;
+        l= lenght;
+        h= hight;
         
         /*to 4*k */{
-            int l=3*this::l;
-            this::add=0;
+            int l=3*l;
+            add=0;
             double k = (double) l/4;
             while( (int)k != k){
-                this::add++;
-                k = ((double)(l+this::add)/4);
+                add++;
+                k = ((double)(l+add)/4);
             }
         }
         
-        this::size=EN_TETE_SIZE+3*lenght*hight+this::add*hight;
+        size=EN_TETE_SIZE+3*lenght*hight+add*hight;
 
         if(img.size>RAM_MAX){
             //Error: size is too big
         }
-        this::hex=malloc(this::size);
-        FILE *fichier=fopen(this::path,"r");
-            this::set_header_image();
+        hex=malloc(size);
+        FILE *fichier=fopen(path,"r");
+            set_header_image();
         fclose(fichier);
     }
 
@@ -58,46 +58,46 @@ Bmp::Bmp(char *img_path, int lenght, int hight){
 void Bmp::set_header_image(){
 	unsigned char p2oct[2]={66,77};
 
-        this::hex[0]=66; this::hex[1]=77; //signature bmp
+        hex[0]=66; hex[1]=77; //signature bmp
 
         unsigned char *p4oct=malloc(4);
         p4oct=adapt(DtoH(img.size));
-        this::hex[2]=p4oct[0]; this::hex[3]=p4oct[1];
-        this::hex[4]=p4oct[2]; this::hex[5]=p4oct[3];//size of file
+        hex[2]=p4oct[0]; hex[3]=p4oct[1];
+        hex[4]=p4oct[2]; hex[5]=p4oct[3];//size of file
 
-        this::hex[6]=0; this::hex[7]=0;
-        this::hex[8]=0; this::hex[9]=0;
+        hex[6]=0; hex[7]=0;
+        hex[8]=0; hex[9]=0;
 
-        this::hex[10]=54; this::hex[11]=0;
-        this::hex[12]=0; this::hex[13]=0;
+        hex[10]=54; hex[11]=0;
+        hex[12]=0; hex[13]=0;
 
-        this::hex[14]=40; this::hex[15]=0;
-        this::hex[16]=0; this::hex[17]=0;
+        hex[14]=40; hex[15]=0;
+        hex[16]=0; hex[17]=0;
 
         p4oct=adapt(DtoH(img.l));
-        this::hex[18]=p4oct[0]; this::hex[19]=p4oct[1];
-        this::hex[20]=p4oct[2]; this::hex[21]=p4oct[3];//lenght
+        hex[18]=p4oct[0]; hex[19]=p4oct[1];
+        hex[20]=p4oct[2]; hex[21]=p4oct[3];//lenght
 
         p4oct=adapt(DtoH(img.h));
-        this::hex[22]=p4oct[0]; this::hex[23]=p4oct[1];
-        this::hex[24]=p4oct[2]; this::hex[25]=p4oct[3];//hight
+        hex[22]=p4oct[0]; hex[23]=p4oct[1];
+        hex[24]=p4oct[2]; hex[25]=p4oct[3];//hight
 
-        this::hex[26]=1; this::hex[27]=0;
-        this::hex[28]=24; this::hex[29]=0;
+        hex[26]=1; hex[27]=0;
+        hex[28]=24; hex[29]=0;
 
-        this::hex[30]=0; this::hex[31]=0;
-        this::hex[32]=0; this::hex[33]=0;
+        hex[30]=0; hex[31]=0;
+        hex[32]=0; hex[33]=0;
 
-        this::hex[34]=24; this::hex[35]=0;
+        hex[34]=24; hex[35]=0;
 
         int i;
         for(i=0;i<18;i++){
-            this::hex[36+i]=0;
+            hex[36+i]=0;
         }
 
         //Inisialize the body of the image to 00
-        for(i=0;i<this::size-EN_TETE_SIZE;i++){
-            this::hex[EN_TETE_SIZE+i]=0;
+        for(i=0;i<size-EN_TETE_SIZE;i++){
+            hex[EN_TETE_SIZE+i]=0;
         }
 };
 
@@ -117,31 +117,31 @@ color Bmp::rgb(unsigned char o1, unsigned char o2, unsigned char o3){
     return c;
 };
 pixel Bmp::get_pixel(int x, int y){
-    pixel px; px.img=this;
-    px.x=x; px.y=y; px.couleur = this::rgb(0,0,0);
+    pixel px; px.img=this;//need modification
+    px.x=x; px.y=y; px.couleur = rgb(0,0,0);
     int offset=from2Dto1D(px);
     color c;
-        c.o3=this::hex[offset];
-        c.o2=this::hex[offset+1];
-        c.o1=this::hex[offset+2];
+        c.o3=hex[offset];
+        c.o2=hex[offset+1];
+        c.o1=hex[offset+2];
     
     px.couleur=c;
 
     return px;
 };
 void Bmp::put_pixel(pixel px){
-        this::hex[from2Dto1D(px)]=px.couleur.o3;
-        this::hex[from2Dto1D(px)+1]=px.couleur.o2;
-        this::hex[from2Dto1D(px)+2]=px.couleur.o1;
+        hex[from2Dto1D(px)]=px.couleur.o3;
+        hex[from2Dto1D(px)+1]=px.couleur.o2;
+        hex[from2Dto1D(px)+2]=px.couleur.o1;
 };
 
 void Bmp::save(){
-    FILE *f=fopen(this::path,"wb");
-        fwrite(this::hex,this::size,1,f);
+    FILE *f=fopen(path,"wb");
+        fwrite(hex,size,1,f);
     fclose(f);
 };
 
-unsigned int Bmp::from2Dto1D(pixel px){
+unsigned int Bmp::from2Dto1D(pixel px){//need modification:
     px.y=px.img::h-px.y-1;
     return EN_TETE_SIZE+3*(px.y*px.img::l+px.x)+px.img::add*px.y;
 };
